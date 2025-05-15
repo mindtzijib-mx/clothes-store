da21efeed9a3b1ebb512662adcd4e831cdc44785
MindTzijib <otiliojr1999@gmail.com>
2025-05-15 17:08:50 -0600

    Support for mobiles

--- js/modules/SliderCategories.js
+++ js/modules/SliderCategories.js
@@ -1,10 +1,10 @@
 export default function sliderCategories() {
   const container = document.querySelector(".categories-container");
-  if (!container) return;
   let isDown = false;
   let startX;
   let scrollLeft;
 
+  // Para PC
   container.addEventListener("mousedown", (e) => {
     isDown = true;
     container.classList.add("active");
@@ -12,21 +12,30 @@ export default function sliderCategories() {
     scrollLeft = container.scrollLeft;
   });
 
-  container.addEventListener("mouseleave", () => {
-    isDown = false;
-    container.classList.remove("active");
-  });
-
-  container.addEventListener("mouseup", () => {
-    isDown = false;
-    container.classList.remove("active");
-  });
+  container.addEventListener("mouseleave", () => (isDown = false));
+  container.addEventListener("mouseup", () => (isDown = false));
 
   container.addEventListener("mousemove", (e) => {
     if (!isDown) return;
     e.preventDefault();
     const x = e.pageX - container.offsetLeft;
-    const walk = (x - startX) * 1.5; // sensibilidad
+    const walk = (x - startX) * 1.5;
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+  // Para móviles
+  container.addEventListener("touchstart", (e) => {
+    isDown = true;
+    startX = e.touches[0].pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("touchend", () => (isDown = false));
+
+  container.addEventListener("touchmove", (e) => {
+    if (!isDown) return;
+    const x = e.touches[0].pageX - container.offsetLeft;
+    const walk = (x - startX) * 1.5;
     container.scrollLeft = scrollLeft - walk;
   });
 }
