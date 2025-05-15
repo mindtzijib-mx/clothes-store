4975fe2281a90ebbd8b31855d49f11e6cae0344f
MindTzijib <otiliojr1999@gmail.com>
2025-05-15 17:01:54 -0600

    Slider Categories Added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import OpenShoppingCart from "./modules/OpenShoppingCart.js";
 import ShoppingCartFunction from "./modules/ShoppingCartFunction.js";
+import sliderCategories from "./modules/SliderCategories.js";
 
 const shoppingCart = new OpenShoppingCart();
 const shoppingCartFunction = new ShoppingCartFunction();
+
+sliderCategories();
---
+++ js/modules/SliderCategories.js
@@ -0,0 +1,32 @@
+export default function sliderCategories() {
+  const container = document.querySelector(".categories-container");
+  if (!container) return;
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  container.addEventListener("mousedown", (e) => {
+    isDown = true;
+    container.classList.add("active");
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("mouseleave", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
+
+  container.addEventListener("mouseup", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 1.5; // sensibilidad
+    container.scrollLeft = scrollLeft - walk;
+  });
+}
